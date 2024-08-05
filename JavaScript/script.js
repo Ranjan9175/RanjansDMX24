7caa849bfd2a4ca881289bcf166de97bd7022836
Ranjan9175 <157478045+Ranjan9175@users.noreply.github.com>
2024-08-05 16:50:16 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+function expand() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
+
+  
